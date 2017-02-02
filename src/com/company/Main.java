10feb9fa7b3f56cde11d4fb5bd4174d2273b999a
Elias Mimouni <emimouni10@hotmail.com>
2017-02-02 10:05:13 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
+import java.text.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kb = new Scanner(System.in);
+
+
+    System.out.println("Enter your phone number:");
+    String number = kb.next();
+    System.out.println("Enter the message you would like to input:");
+    String text = kb.next();
+
+    DateFormat df = new SimpleDateFormat("HH:mm:ss");
+    Date dateobj = new Date();
+        //System.out.println(df.format(dateobj));
     }
 }
