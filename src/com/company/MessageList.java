bc832e21d35fee5ea0357a76117971f06b014b99
Elias Mimouni <emimouni10@hotmail.com>
2017-02-02 10:04:50 -0500

    Create MessageList.java

---
+++ src/com/company/MessageList.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.ArrayList;
+
+/**
+ * Created by emimo on 2/1/2017.
+ */
+public class MessageList {
+    public MessageList(String number){
+    }
+
+    public  ArrayList user1 = new ArrayList();
+    public  ArrayList user2 = new ArrayList();
+
+    public static void add(){
+
+    }
+    public static void moveMessage(int position, MessageList ml){
+
+    }
+}
