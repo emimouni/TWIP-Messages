5c36d8879dc083073df3b959201a725b7a5cec74
Elias Mimouni <emimouni10@hotmail.com>
2017-02-03 06:25:06 -0500

    Update Messages.java

--- src/com/company/Messages.java
+++ src/com/company/Messages.java
@@ -1,9 +1,38 @@
 package com.company;
+import java.text.*;
+import java.util.*;
 
 /**
  * Created by emimo on 2/1/2017.
  */
 public class Messages {
-    public Messages(String number, String text){
+    Date time;
+    public String num,text;
+    public Messages(String number, String mes){
+        num = number;
+        text = mes;
+        time = new Date(System.currentTimeMillis());
+    }
+
+    public Date getTheTime(){
+        return time;
+    }
+
+    public String getTheRealTime(){
+        DateFormat x = new SimpleDateFormat("HH:mm");
+        return x.format(time);
+    }
+
+    public void changeUserInfo(String x, String y){
+        num = x;
+        text = y;
+    }
+
+    public String getPhoneNumber(){
+        return num;
+    }
+
+    public String getMessage(){
+        return text;
     }
 }
