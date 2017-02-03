622a683c2686341b1826a90848cb506cbbb0131c
Elias Mimouni <emimouni10@hotmail.com>
2017-02-03 06:24:23 -0500

    Update MessageList.java

--- src/com/company/MessageList.java
+++ src/com/company/MessageList.java
@@ -1,21 +1,62 @@
 package com.company;
 
+import com.intellij.openapi.compiler.CompilationException;
+
 import java.util.ArrayList;
 
 /**
  * Created by emimo on 2/1/2017.
  */
 public class MessageList {
-    public MessageList(String number){
+    public String number;
+    public ArrayList<Messages> texts = new ArrayList<Messages>();
+
+    public MessageList(String num){
+        number = num;
     }
 
-    public  ArrayList user1 = new ArrayList();
-    public  ArrayList user2 = new ArrayList();
-
-    public static void add(){
-
+    public void add(Messages mes){
+        long time = mes.getTheTime().getTime();
+        int i = 0;
+        for (Messages msg: texts){
+            if (time > msg.getTheTime().getTime()){
+                i++;
+            }
+        }
+        texts.add(i, mes);
     }
-    public static void moveMessage(int position, MessageList ml){
 
+    public void remove(int position){
+        texts.remove(position);
     }
+
+    public void clear(){
+        texts.clear();
+    }
+
+    public Messages get(int position){
+        return texts.get(position);
+    }
+
+    public void move(int position, MessageList ml1, MessageList ml2){
+        if (ml1 != null){
+            ml1.add(ml2.get(position));
+            ml2.remove(position);
+        }
+        else {
+            System.out.println("The message list that you are referring to does not exist.");
+        }
+    }
+
+    public void displayMessages(){
+        System.out.println("Messages belonging to: " + number);
+        for (Messages m: texts){
+            System.out.println(m.getMessage() + "\tTime Received: " + m.getTheRealTime());
+        }
+    }
+
+    public String getNum(){
+        return number;
+    }
+
 }
